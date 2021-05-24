d5552ede2e7ed7f7472bf6217ac626a05f5949db
AnteAntonini <anteantonini.mtk@hotmail.com>
2021-05-24 19:03:09 +0200

    add relations

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    recruiter_id: { type: Schema.Types.ObjectId, ref: 'Recruiter'},
     first_name: String,
     last_name: String,
     email: String,
